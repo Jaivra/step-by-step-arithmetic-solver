08b0a3a306e7a6723cfebd05df85d61ef06a4e06
jaivra <Valecisla98>
2021-09-06 15:13:01 +0200

    synchro

--- block_depth.py
+++ block_depth.py
@@ -8,15 +8,19 @@ class BlockDepth:
     INIT = 0
     _count = itertools.count(0)
 
-    def __init__(self, block_type = INIT, depth = 0, pos = 0):
+    def __init__(self, block_type=INIT, depth=0, pos=0):
         self._block_type = block_type
         self._depth = depth
         self._pos = pos
 
-
     def add_block(self, block_type):
-        return BlockDepth(max(block_type, self._block_type), self._depth + 1, next(BlockDepth._count))
+        depth = self._depth
+        if self._block_type == block_type == self.ROUND:
+            depth += 1
+        elif self._block_type >= block_type:
+            raise Exception('Error parenthesis')
 
+        return BlockDepth(block_type, depth, next(BlockDepth._count))
 
     @property
     def block_type(self):
@@ -30,8 +34,10 @@ class BlockDepth:
     def pos(self):
         return self._pos
 
-
     def __gt__(self, other):
-        return self._block_type > other.block_type or\
-               ( self._block_type == other.block_type and self._depth > other.depth) or\
+        return self._block_type > other.block_type or \
+               (self._block_type == other.block_type and self._depth > other.depth) or \
                (self._block_type == other.block_type and self._depth == other.depth and self._pos < other.pos)
+
+    def __repr__(self):
+        return f'{self._block_type}{self._depth}{self._pos}'
